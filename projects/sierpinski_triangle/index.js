d7b6408209f26dc49210945e26072067c162ecc1
Minotriz02 <sebastian.lopez_Ast@uao.edu.com>
2024-01-26 16:30:22 -0500

    sky star and triangle sierpinski

---
+++ projects/sierpinski_triangle/index.js
@@ -0,0 +1,84 @@
+const canvas = document.getElementById("my-canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.radius = 1;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+const triangleTop = new Point(window.innerWidth / 2, 50);
+triangleTop.draw();
+
+const triangleBottomLeft = new Point(50, window.innerHeight - 50);
+triangleBottomLeft.draw();
+
+const triangleBottomRight = new Point(
+  window.innerWidth - 50,
+  window.innerHeight - 50
+);
+triangleBottomRight.draw();
+
+const triangle = [triangleTop, triangleBottomLeft, triangleBottomRight];
+
+const getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const drawRest = (prevPoint) => {
+  let randomCorner, middlePoint;
+  for (let i = 0; i < 10000; i++) {
+    randomCorner = triangle[getRandomInt(0, 3)];
+    middlePoint = new Point(
+      (prevPoint.x + randomCorner.x) / 2,
+      (prevPoint.y + randomCorner.y) / 2
+    );
+    middlePoint.draw();
+    prevPoint = middlePoint;
+  }
+};
+
+canvas.addEventListener(
+  "click",
+  (e) => {
+    const firstPoint = new Point(e.x, e.y);
+    firstPoint.draw();
+    drawRest(firstPoint);
+  },
+  { once: true }
+);
+// let points = [
+//   new Point(canvas.width / 2, canvas.height / 2 - 100),
+//   new Point(canvas.width / 2 + 100, canvas.height / 2 + 100),
+//   new Point(canvas.width / 2 - 100, canvas.height / 2 + 100),
+// ];
+
+// const start = () => {
+//   points.forEach((point) => {
+//     point.draw();
+//     console.log(point);
+//   });
+// };
+
+// start();
+
+// const animate = () => {
+//   for (let i = 0; i < 200; i++) {
+//     if (i === 0) {
+//       const newPoint = new Point(Math.random()*canvas.width, Math.random()*canvas.height);
+//     }
+//     let randomPoint = points[Math.floor(Math.random() * points.length)];
+//   }
+// };
+
+// animate();
